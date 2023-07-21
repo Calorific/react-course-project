efc2081e4141a6e7b6e5d422af312107eee77646
Sergey Skorykh <kknd3390@mail.ru>
2023-07-22 01:50:45 +0700

    added basic routing and each user page

---
+++ src/components/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Main = () => {
+
+  return (
+    <>
+      <h2>Main Page</h2>
+    </>
+  )
+}
+
+Main.propTypes = {
+
+}
+
+export default Main
