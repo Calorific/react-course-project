aedee886a9c6f0e109d888c2f2dffa5a8422f12e
Sergey Skorykh <kknd3390@mail.ru>
2023-07-21 22:09:41 +0700

    added sorting in table

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Users from './components/users'
-import api from './API'
 
 
 const App = () => {
-  const [users, setUsers] = useState()
-
-  useEffect(() => {
-    api.users.fetchAll().then(data => setUsers(data))
-  }, [])r
-
-  const handleDelete = id => {
-    setUsers(users.filter(user => user._id !== id))
-  }
-
-  const handleMarking = id => {
-    const allUsers = [...users]
-    const user = allUsers.find(u => u._id === id)
-    user.bookmark = !user.bookmark
-
-    setUsers(allUsers)
-  }
-
   return (
     <>
       <div className="App">
-        {users && <Users users={users} onDelete={handleDelete} onMarking={handleMarking} />}
+        <Users />
       </div>
     </>
 
