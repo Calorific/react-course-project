701cd82b6d4d781e12db3318437266f34ec436e3
Calorific <128981793+Calorific@users.noreply.github.com>
2023-07-31 23:15:46 +0700

    Update commentForm.jsx

--- src/components/common/comments/commentForm.jsx
+++ src/components/common/comments/commentForm.jsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import TextareaField from '../../common/form/textareaField'
+import TextareaField from '../form/textareaField'
 import api from '../../../API'
 import { validator } from '../../../utils/validator'
-import SelectField from '../../common/form/selectField'
+import SelectField from '../form/selectField'
 
 const CommentForm = ({ id, onAdd }) => {
-  const [data, setData] = useState({
-    user: '',
-    content: ''
-  })
+  const initialState = { user: '', content: '' }
+
+  const [data, setData] = useState(initialState)
+
   const [users, setUsers] = useState([])
   const [errors, setErrors] = useState({})
 
   useEffect(() => {
     api.users.fetchAll().then(data => setUsers(data.map(u => ({ value: u._id, name: u.name }))))
-    validate()
   }, [])
 
   useEffect(() => {
@@ -44,6 +43,8 @@ const CommentForm = ({ id, onAdd }) => {
   const handleSubmit = e => {
     e.preventDefault()
     if (!validate()) return
+    setData(initialState)
+    setErrors({})
     api.comments.add({ pageId: id, userId: data.user, content: data.content }).then(comment => onAdd(comment))
   }
 
@@ -57,7 +58,7 @@ const CommentForm = ({ id, onAdd }) => {
             <SelectField onChange={handleChange} name='user' value={data.user} defaultOption='Выберите пользователя'
               options={users} error={errors.user} />
             <TextareaField name='content' value={data.content} onChange={handleChange} label='Сообщение'
-              error={errors.content} />
+              error={errors.content} rows={3} />
             <div className="d-flex justify-content-end">
               <button className="btn btn-primary " onClick={handleSubmit} type='submit' disabled={!isValid}>
                 Опубликовать
