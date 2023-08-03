bddf69984988c1476770c1713cd68211e96a1b39
Sergey Skorykh <kknd3390@mail.ru>
2023-08-03 19:59:24 +0700

    quality refactoring

--- src/components/ui/qualities/quality.jsx
+++ src/components/ui/qualities/quality.jsx
@@ -3,14 +3,11 @@ import PropTypes from 'prop-types'
 import { useQualities } from '../../../hooks/useQualities'
 
 const Quality = ({ qualityId }) => {
-  const { loading, getQuality } = useQualities()
+  const { getQuality } = useQualities()
   const quality = getQuality(qualityId)
   return (
       <>
-        {!loading
-          ? <span className={'badge m-1  bg-' + quality.color}>{quality.name}</span>
-          : 'Loading...'
-        }
+         <span className={'badge m-1  bg-' + quality.color}>{quality.name}</span>
       </>
   )
 }
